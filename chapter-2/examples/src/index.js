22f3a37e9a6408c055bfb246cdb82ea0de6f4419
MaratFa <evolution99@rambler.ru>
2024-08-03 14:47:28 +0300

    edited

--- chapter-2/examples/src/index.js
+++ chapter-2/examples/src/index.js
@@ -1,9 +1,18 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+
+function Advertisement() {
+  return React.createElement(
+    "a",
+    { href: "https://my-website.com" },
+    "Visit my website"
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h2>Hello World!</h2>
+    <Advertisement />
     <p>Welcome to this book!</p>
   </div>,
   document.getElementById("root")
