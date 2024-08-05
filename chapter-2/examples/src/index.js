6af0c4a29ea0c33bc716f4f73a42e7d2ba010991
MaratFa <evolution99@rambler.ru>
2024-08-05 07:52:06 +0300

    edited

--- chapter-2/examples/src/index.js
+++ chapter-2/examples/src/index.js
@@ -2,17 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return React.createElement("p", {}, [
-    "Please visit my ",
-    React.createElement("a", { href: "https://my-blog-site.com" }, "Blog"),
-  ]);
+  const userName = "Max";
+  return <p>Hi, my name is {`super ${userName}`}!</p>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <App />
-    <p>Welcome to this book!</p>
-  </div>,
-  document.getElementById("root")
-);
+root.render(<App />);
