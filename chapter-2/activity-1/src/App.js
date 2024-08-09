4ab4d75d0db9dba9ba45c5924466c9634b9904ae
MaratFa <evolution99@rambler.ru>
2024-08-10 00:42:47 +0300

    solution

--- chapter-2/activity-1/src/App.js
+++ chapter-2/activity-1/src/App.js
@@ -2,20 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <h1>Hi, this is me - John!</h1>
-      <p>Rigt now, I am 34 years old and I live in Berlin</p>
-      <p>My full name is John Johnson and I am a web developer. ddd</p>
+    <div className="app-container">
+      <h1 className="app-title">Hi, this is me - John!</h1>
+      <p className="app-text">
+        Right now, I am 34 years old and I live in Berlin
+      </p>
+      <p className="app-text">
+        My full name is John Johnson and I am a web developer.
+      </p>
     </div>
   );
 }
-
 
 export default App;
