5448f66c8051fe4012d1705b879897502e917a96
MaratFa <evolution99@rambler.ru>
2024-08-11 18:38:47 +0300

    edited

--- chapter-2/activity-1/src/App.js
+++ chapter-2/activity-1/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="app-container">
       <h1 className="app-title">Hi, this is me - John!</h1>
       <p className="app-text">
-        Right now, I am 34 years old and I live in Berlin
+        Right now, I am 34 years old and I live in Berlin.
       </p>
       <p className="app-text">
         My full name is John Johnson and I am a web developer.
