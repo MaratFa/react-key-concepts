d0a34bbb417e8c6f4eeacc2fce90c686eec1614f
MaratFa <evolution99@rambler.ru>
2024-08-16 22:43:09 +0300

    added

---
+++ chapter-3/examples/src/components/GoalItem.js
@@ -0,0 +1,10 @@
+function GoalItem() {
+  return (
+    <li>
+      <h2>Finish the React tutorial</h2>
+      <p>Learn React and build a simple app!</p>
+    </li>
+  );
+}
+
+export default GoalItem;
