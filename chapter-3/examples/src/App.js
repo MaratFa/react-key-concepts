49b556406a49d6c27f06fe3947debeb32b9d3035
MaratFa <evolution99@rambler.ru>
2024-08-18 02:40:44 +0300

    edited

--- chapter-3/examples/src/App.js
+++ chapter-3/examples/src/App.js
@@ -4,7 +4,7 @@ import GoalItem from "./components/GoalItem";
 function App() {
   return (
     <ul>
-      <GoalItem id="q1" title="Finish the book" />
+      <GoalItem id="q1" title="Finish the book" >{33}</GoalItem>
       <GoalItem id="g2" title="Learn all about React" />
     </ul>
   );
