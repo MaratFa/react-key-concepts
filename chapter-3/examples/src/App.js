5de5c5f656528bdcf0490d29e25175560f05d12d
MaratFa <evolution99@rambler.ru>
2024-08-19 09:27:39 +0300

    added

--- chapter-3/examples/src/App.js
+++ chapter-3/examples/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import GoalItem from "./components/GoalItem";
+import Product from "./components/Product";
+
+const productData = { title: "A book", price: 29.99, id: "p1" };
 
 function App() {
   return (
     <ul>
-      <GoalItem id="q1" title="Finish the book" >{33}</GoalItem>
-      <GoalItem id="g2" title="Learn all about React" />
+      <GoalItem id="q1" title="Finish the book" />
+      <GoalItem id="g2">Learn all about React</GoalItem>
+      <Product title="A book" price={29.99} id="p1" />
     </ul>
   );
 }
