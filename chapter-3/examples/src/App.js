cbe6477f74042f41860d6e8c6d3e9892444c011a
MaratFa <evolution99@rambler.ru>
2024-08-27 12:42:52 +0300

    edited

--- chapter-3/examples/src/App.js
+++ chapter-3/examples/src/App.js
@@ -9,10 +9,9 @@ function App() {
     <ul>
       <GoalItem id="q1" title="Finish the book" />
       <GoalItem id="g2">Learn all about React</GoalItem>
-      <Product title="A book" price={29.99} id="p1" />
+      <Product {...productData}>3sdafgsdf</Product>
     </ul>
   );
 }
 
 export default App;
-
