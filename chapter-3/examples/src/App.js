51f65c97daceef3a29cd1fb80558709b4142d152
MaratFa <evolution99@rambler.ru>
2024-09-23 00:06:45 +0300

    edited

--- chapter-3/examples/src/App.js
+++ chapter-3/examples/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import GoalItem from "./components/GoalItem";
+import Link from "./components/Link";
 import Product from "./components/Product";
 
 const productData = { title: "A book", price: 29.99, id: "p1" };
@@ -10,6 +11,7 @@ function App() {
       <GoalItem id="q1" title="Finish the book" />
       <GoalItem id="g2">Learn all about React</GoalItem>
       <Product {...productData}>3sdafgsdf</Product>
+      <Link>Link</Link>
     </ul>
   );
 }
